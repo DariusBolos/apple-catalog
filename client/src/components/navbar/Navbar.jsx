49a2c48dab10164e3c79ce035e7d60dc1766b9ec
Darius Bolos <Darius.Bolos@garmin.com>
2025-07-16 10:55:00 +0300

    signout button implementation

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,6 +4,12 @@ import logo from '../../assets/apple-seeklogo.png';
 
 const Navbar = () => {
     const lang = localStorage.getItem('i18nextLng');
+    const token = localStorage.getItem('token');
+
+    const handleSignOut = () => {
+        localStorage.removeItem('token');
+        window.location.href = '/';
+    };
 
     return (
         <nav className="navbar">
@@ -11,7 +17,14 @@ const Navbar = () => {
                 <div className="navbar-logo">
                     <img alt="apple logo" src={logo} width={45} />
                 </div>
-                <ul className="navbar-links">{<LanguageSelector currentLang={lang} />}</ul>
+                <ul className="navbar-links">
+                    <LanguageSelector currentLang={lang} />
+                    {!!token && (
+                        <button className="btn-signout" onClick={handleSignOut}>
+                            Sign out
+                        </button>
+                    )}
+                </ul>
             </div>
         </nav>
     );
